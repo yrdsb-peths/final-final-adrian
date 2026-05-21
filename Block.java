207f8ff5729bfeb991f24b37748099a11e400538
348818030 <348818030@gapps.yrdsb.ca>
2026-05-21 11:36:51 -0400

    Add block class with animations

---
+++ Block.java
@@ -0,0 +1,54 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Block here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Block extends Actor
+{
+    GreenfootImage [] expression = new GreenfootImage[2];
+    SimpleTimer animationTimer = new SimpleTimer();
+    
+    private String blockType;
+    
+    /**
+     * Act - do whatever the Block wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Block(String image)
+    {
+        for(int i = 0; i < expression.length; i++)
+        {
+            blockType = image;
+            expression[i] = new GreenfootImage("images/" + blockType + i + ".png");
+            expression[i].scale(50,50);
+        }
+        
+        animationTimer.mark();
+        
+        setImage(expression[0]);
+    }
+    
+    // Animate the block
+    int imageIndex = 0;
+    public void animateBlock()
+    {
+        if(animationTimer.millisElapsed() < 1000)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
+        setImage(expression[imageIndex]);
+        imageIndex = (imageIndex + 1) % expression.length;
+    
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+        animateBlock();
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -6,5 +6,8 @@ public class MyWorld extends World {
         
         Player ball = new Player();
         addObject(ball, 300, 420);
+        
+        Block obstacle = new Block("Red");
+        addObject(obstacle, 300, 200);
     }
 }
