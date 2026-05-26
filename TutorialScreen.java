61aa3cf23b7756ada1fc25d6a20403870ab3c4d9
348818030 <348818030@gapps.yrdsb.ca>
2026-05-26 11:07:31 -0400

    Add new tutorial screen world

---
+++ TutorialScreen.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TutorialScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TutorialScreen extends World
+{
+
+    /**
+     * Constructor for objects of class TutorialScreen.
+     * 
+     */
+    public TutorialScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(560, 720, 1); 
+    }
+}
