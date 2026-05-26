dc1f8628e45d2f87bf3209a126e169177d77a911
348818030 <348818030@gapps.yrdsb.ca>
2026-05-26 11:10:55 -0400

    Set background of tutorialScreen world

--- TutorialScreen.java
+++ TutorialScreen.java
@@ -17,5 +17,9 @@ public class TutorialScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(560, 720, 1); 
+        
+        // Set background colour
+        getBackground().setColor(new Color(199, 174, 210));
+        getBackground().fill();
     }
 }
