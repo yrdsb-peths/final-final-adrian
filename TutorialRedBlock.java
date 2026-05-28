4f3979b047a213735c522c230eda2cc7d06af293
348818030 <348818030@gapps.yrdsb.ca>
2026-05-28 11:11:38 -0400

    Add new tutorialRedBlock subclass of block to use as an example in the tutorial. It doesn't end game when it touches the player.

---
+++ TutorialRedBlock.java
@@ -0,0 +1,73 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TutorialRedBlock here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TutorialRedBlock extends Block
+{
+    public TutorialRedBlock(String blockType, int speed)
+    {
+        super(blockType, speed);
+    }
+    
+    /**
+     * Act - do whatever the TutorialRedBlock wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        if (checkCollision()) 
+        {
+            return; 
+        }
+        
+        if(removeIfOffScreen())
+        {
+            return;
+        } 
+        animateBlock();
+        fall();
+        rotateBlock();
+        updateBlock();
+    }
+    
+    public boolean checkCollision()
+    {
+        // Gets this world
+        World world = getWorld();
+        // Gets all players in this world (1) and stores it into a array and then get the first player in the array
+        Player player = (Player)world.getObjects(Player.class).get(0);
+        
+        // Gets the horizontal distance between the player and the block
+        int xDistance = getX() - player.getX();
+        // Gets the vertical distance between the player and the block
+        int yDistance = getY() - player.getY();
+        
+        // Gets the shortest distance between the player and the block
+        double distance = (xDistance * xDistance) + (yDistance * yDistance);
+        distance = Math.sqrt(distance);
+        
+        // Creates a list of all players
+        java.util.List<Player> players = world.getObjects(Player.class);
+        
+        // Ensures a player exists
+        if(players.size() > 0)
+        {
+            // If the distance between the objects is less than a certain distance then the game ends and returns true 
+            // to tell the game that the object is removed
+            if(distance < 45)
+            {
+                world.removeObject(this);
+                return true;
+            }
+        }
+        
+        // If object is not removed
+        return false;
+        
+    }
+}
